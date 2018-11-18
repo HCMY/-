7bf1f3af4ebd9004748ca5282a438f616f0f848c
zhouyc <yuanchenzhouhcmy@gmail.com>
2018-11-18 11:37:46 +0800

    Create huffman.c
    huffman  encoding and decoding
    哈夫曼数编码解码，严蔚敏

---
+++ DataStructure/Tree/huffman.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int ELEMTYPE;
+
+// 哈夫曼树结点结构体
+typedef struct HuffmanTree
+{
+    ELEMTYPE weight;
+    ELEMTYPE id;        // id用来主要用以区分权值相同的结点，这里代表了下标
+    struct HuffmanTree* lchild;
+    struct HuffmanTree* rchild;
+}HuffmanNode;
+
+// 构建哈夫曼树
+HuffmanNode* createHuffmanTree(int* a, int n)
+{
+    int i, j;
+    HuffmanNode **temp, *hufmTree;
+    temp = malloc(n*sizeof(HuffmanNode));
+    for (i=0; i<n; ++i)     // 将数组a中的权值赋给结点中的weight
+    {
+        temp[i] = (HuffmanNode*)malloc(sizeof(HuffmanNode));
+        temp[i]->weight = a[i];
+        temp[i]->id = i;
+        temp[i]->lchild = temp[i]->rchild = NULL;
+    }
+
+    for (i=0; i<n-1; ++i)       // 构建哈夫曼树需要n-1合并
+    {
+        int small1=-1, small2;      // small1、small2分别作为最小和次小权值的下标
+        for (j=0; j<n; ++j)         // 先将最小的两个下标赋给small1、small2（注意：对应权值未必最小）
+        {
+            if (temp[j] != NULL && small1==-1)
+            {
+                small1 = j;
+                continue;
+            } else if(temp[j] != NULL)
+            {
+                small2 = j;
+                break;
+            }
+        }
+
+        for (j=small2; j<n; ++j)    // 比较权值，挪动small1和small2使之分别成为最小和次小权值的下标
+        {
+            if (temp[j] != NULL)
+            {
+                if (temp[j]->weight < temp[small1]->weight)
+                {
+                    small2 = small1;
+                    small1 = j;
+                } else if (temp[j]->weight < temp[small2]->weight)
+                {
+                    small2 = j;
+                }
+            }
+        }
+        hufmTree = (HuffmanNode*)malloc(sizeof(HuffmanNode));
+        hufmTree->weight = temp[small1]->weight + temp[small2]->weight;
+        hufmTree->lchild = temp[small1];
+        hufmTree->rchild = temp[small2];
+
+        temp[small1] = hufmTree;
+        temp[small2] = NULL;
+    }
+    free(temp);
+    return hufmTree;
+}
+
+// 以广义表的形式打印哈夫曼树
+void PrintHuffmanTree(HuffmanNode* hufmTree)
+{
+    if (hufmTree)
+    {
+        printf("%d", hufmTree->weight);
+        if (hufmTree->lchild != NULL || hufmTree->rchild != NULL)
+        {
+            printf("(");
+            PrintHuffmanTree(hufmTree->lchild);
+            printf(",");
+            PrintHuffmanTree(hufmTree->rchild);
+            printf(")");
+        }
+    }
+}
+
+// 递归进行哈夫曼编码
+void HuffmanCode(HuffmanNode* hufmTree, int depth)      // depth是哈夫曼树的深度
+{
+    static int code[10];
+    if (hufmTree)
+    {
+        if (hufmTree->lchild==NULL && hufmTree->rchild==NULL)
+        {
+            printf("id:%d weigh: %d encode:", hufmTree->id, hufmTree->weight);
+            int i;
+            for (i=0; i<depth; ++i)
+            {
+                printf("%d", code[i]);
+            }
+            printf("\n");
+        } else
+        {
+            code[depth] = 0;
+            HuffmanCode(hufmTree->lchild, depth+1);
+            code[depth] = 1;
+            HuffmanCode(hufmTree->rchild, depth+1);
+        }
+    }
+}
+
+// 哈夫曼解码
+void HuffmanDecode(char ch[], HuffmanNode* hufmTree, char string[])     // ch是要解码的01串，string是结点对应的字符
+{
+    int i;
+    int num[100];
+    HuffmanNode* tempTree = NULL;
+    for (i=0; i<strlen(ch); ++i)
+    {
+        if (ch[i] == '0')
+            num[i] = 0;
+        else
+            num[i] = 1;
+    }
+    if(hufmTree)
+    {
+        i = 0;      // 计数已解码01串的长度
+        while(i<strlen(ch))
+        {
+            tempTree = hufmTree;
+            while(tempTree->lchild!=NULL && tempTree->rchild!=NULL)
+            {
+                if (num[i] == 0)
+                {
+                    tempTree = tempTree->lchild;
+                } else
+                {
+                    tempTree = tempTree->rchild;
+                }
+                ++i;
+            }
+            printf("%c", string[tempTree->id]);     // 输出解码后对应结点的字符
+        }
+    }
+}
+
+int main()
+{
+    int i, n;
+    printf("input the number of leaf nodes:\n");
+    while(1)
+    {
+        scanf("%d", &n);
+        if (n>1)
+            break;
+        else
+            printf("type error, try again");
+    }
+
+    int* arr;
+    arr=(int*)malloc(n*sizeof(ELEMTYPE));
+    printf("input %d the each weights of leaf nodes:\n", n);
+    for (i=0; i<n; ++i)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    char ch[100], string[100];
+    printf("input %d characters represented by the leaf nodes:\n", n);
+    fflush(stdin);      // 强行清除缓存中的数据，也就是上面输入权值结束时的回车符
+    gets(string);
+
+    HuffmanNode* hufmTree = NULL;
+    hufmTree = createHuffmanTree(arr, n);
+
+    printf("The generalized table form of this Huffman tree is:\n");
+    PrintHuffmanTree(hufmTree);
+    printf("\nThe Huffman code for each leaf node is:\n");
+    HuffmanCode(hufmTree, 0);
+
+    printf("wanna decode, input the codes:\n");
+    gets(ch);
+    printf("decode results:\n");
+    HuffmanDecode(ch, hufmTree, string);
+    printf("\n");
+
+    free(arr);
+    free(hufmTree);
+
+    return 0;
+}
